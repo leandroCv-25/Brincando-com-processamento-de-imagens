9470017a4151df35029d4beaadf370c1d9ca994f
leandroCv-25 <56126184+leandroCv-25@users.noreply.github.com>
2021-09-08 20:10:52 -0300

    Convoluções

---
+++ brincadeira 3 - convoluções/convolucoes.py
@@ -0,0 +1,85 @@
+# Apply convolve
+import numpy as np
+from PIL import Image
+from scipy import ndimage
+
+def main():
+    # Open image
+    image = Image.open('images/img.jpg')
+    image.show()
+
+    # convert image to numpy array    
+    npImage = np.array(image) 
+    #kernel
+    k = np.array([[0,0,0],[0,1,0],[0,0,0]])
+    npImage = ndimage.convolve(npImage,k, cval=0.0)
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/a.jpg')
+    
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[1,0,-1],[0,0,0],[-1,0,1]])
+    npImage = ndimage.convolve(npImage,k, cval=0.0, mode='nearest')
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/b.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[0,-1,0],[-1,4,-1],[0,-1,0]])
+    npImage = ndimage.convolve(npImage,k, mode='reflect')
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/c.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    npImage = ndimage.convolve(npImage,k, mode='reflect')
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/d.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+    npImage = ndimage.convolve(npImage,k, mode='reflect')
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/e.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[1/9,1/9,1/9],[1/9,1/9,1/9],[1/9,1/9,1/9]])
+    npImage = ndimage.convolve(npImage,k, mode='constant', cval=0.0,)
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/f.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[1/16,1/8,1/16],[1/8,1/4,1/8],[1/16,1/8,1/16]])
+    npImage = ndimage.convolve(npImage,k, mode='constant', cval=0.0,)
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/g.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[1/256,4/256,6/256,4/256,1/256],[4/256,16/256,24/256,16/256,4/256],[6/256,24/256,36/256,24/256,6/256],[4/256,16/256,24/256,16/256,4/256],[1/256,4/256,6/256,4/256,1/256]])
+    npImage = ndimage.convolve(npImage,k, mode='constant', cval=0.0,)
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/h.jpg')
+
+    # convert image to numpy array   
+    npImage = np.array(image) 
+    k = np.array([[-1/256,-4/256,-6/256,-4/256,-1/256],[-4/256,-16/256,-24/256,-16/256,-4/256],[-6/256,-24/256,476/256,-24/256,-6/256],[-4/256,-16/256,-24/256,-16/256,-4/256],[-1/256,-4/256,-6/256,-4/256,-1/256]])
+    npImage = ndimage.convolve(npImage,k, mode='constant', cval=0.0,)
+    image2 = Image.fromarray(npImage)
+    image2.show()
+    image2.save('images/i.jpg')
+
+if __name__ == "__main__":
+    main()
